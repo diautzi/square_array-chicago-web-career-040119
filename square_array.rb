09a68a69e070400ea6c5c5cdbb2e876b8411ea92
Diana Utan <didiutzi@gmail.com>
2020-11-28 17:08:54 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,9 @@
-def square_array(array)
-  # your code here
-  new_array= []
-  array.each { |i| new_array.push(i ** 2)}
-  return new_array
-end
+# def square_array(array)
+#   # your code here
+#   new_array= []
+#   array.each { |i| new_array.push(i ** 2)}
+#   return new_array
+# end
 
 # def square_array(array)
 #   puts array.collect {|i| i**2}
